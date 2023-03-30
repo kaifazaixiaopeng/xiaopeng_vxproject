43202d1573d0a26bdb96f6749e0e0637e587acb6
wzp <xiaopengaijava@foxmail.com>
2023-03-30 17:02:50 +0800

    20230330

--- src/main/java/com/xiaopeng/vx/module/controller/TestController.java
+++ src/main/java/com/xiaopeng/vx/module/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.xiaopeng.vx.module.controller;
 
+import cn.hutool.crypto.SecureUtil;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 /**
  * @ClassName: TestController
@@ -17,5 +21,26 @@ public class TestController {
     public String test1(){
         return "helloPeng";
     }
+    // 微信页面填写的token，必须保密
+    private static final String TOKEN = "143148@qq";
 
+    @GetMapping("validate")
+    public String validate(String signature,String timestamp,String nonce,String echostr){
+        // 1. 将token、timestamp、nonce三个参数进行字典序排序
+        String[] arr = {timestamp, nonce, TOKEN};
+        Arrays.sort(arr);
+        // 2. 将三个参数字符串拼接成一个字符串进行sha1加密
+        StringBuilder sb = new StringBuilder();
+        for (String temp : arr) {
+            sb.append(temp);
+        }
+        // 这里利用了hutool的加密工具类
+        String sha1 = SecureUtil.sha1(sb.toString());
+        // 3. 加密后的字符串与signature对比，如果相同则该请求来源于微信，原样返回echostr
+        if (sha1.equals(signature)){
+            return echostr;
+        }
+        // 接入失败
+        return null;
+    }
 }
